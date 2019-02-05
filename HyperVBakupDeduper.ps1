27365b19fe2c09d53e58ebbd310924dca7741f3f
Peter Kleinert <pkleinert@gmail.com>
2019-02-05 01:26:00 +0100

    Powershell to deduplicate Hyper-V backups
    Base and child can be on different drives
    Example: .\dedup2.ps1 20190204-0105 E:\Hyper-V-Bak\SKI-SK-H001 D:\Hyper-V-Monthly\SKI-SK-H001

--- HyperVBakupDeduper.ps1
+++ HyperVBakupDeduper.ps1
@@ -1,25 +1,30 @@
 param (
-    [Parameter(Mandatory=$true)][string]$DedupLast
+    [Parameter(Mandatory=$true)][string]$DedupLast,
+    [Parameter(Mandatory=$true)][string]$BackupFolder,
+    [string]$BaseFolder=$BackupFolder,
+    [string]$DeduperExe=".\Deduper.exe",
+    [string]$DedupBase=$(Get-Content ($BaseFolder+'\dedup_last_full.txt') -TotalCount 1)
 )
 
-$DedupBase=(Get-Content .\dedup_last_full.txt -TotalCount 1)
-
-echo "Deduplication base: $DedupBase"
-echo "Deduplication last: $DedupLast"
+echo "Base root:    $BaseFolder"
+echo "Base folder:  $DedupBase"
+echo "Executable:   $DeduperExe"
+echo "Child root:   $BackupFolder"
+echo "Child folder: $DedupLast"
 
 echo "Deduplicating VHDX disks:"
 $DiskPaths=(Get-ChildItem -Recurse -Path "$BackupFolder\$DedupLast" -File -Filter "*.vhdx" -Name)
 
 foreach($DiskPath in $diskPaths) {
-  $FileBase="$DedupBase\$DiskPath"
+  echo ("{0:yyyy-MM-dd hh:mm:ss}" -f (get-date))
+  $FileBase="$BaseFolder\$DedupBase\$DiskPath"
   $FileLast="$DedupLast\$DiskPath"
-  $FileDedup=([IO.Path]::GetDirectoryName($FileLast) + '\' + [IO.Path]::GetFileNameWithoutExtension($FileLast) + ".ded")
+  $FileDedup=([IO.Path]::GetDirectoryName($FileLast) + '\' + [IO.Path]::GetFileName($FileLast) + ".dex")
 
-  if (Test-Path "$BackupFolder\$DedupBase\$DiskPath" ) {
-    echo "$FileBase # $FileLast -> $FileDedup"
-    .\Deduper.exe -d "$FileBase" "$FileLast" "$FileDedup"
+  if (Test-Path $FileBase ) {
+    echo ";) $FileBase # $FileLast -> $FileDedup"
+    &($DeduperExe) -sd (Resolve-Path($FileBase)) (Resolve-Path($FileLast)) (Resolve-Path($FileDedup))
   } else {
     echo ":( $FileBase"
   }
 }
-
