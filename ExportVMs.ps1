44e1b575a32709aa64a2ca68aca44b85a6e4e5fe
Peter Kleinert <pkleinert@gmail.com>
2019-03-06 23:43:50 +0100

    Parameters, deduplication and new base folder creation w hashing support

--- ExportVMs.ps1
+++ ExportVMs.ps1
@@ -1,84 +1,133 @@
-$ExportPath="F:\Hyper-V-Bak"
-$DedupBase='G:\Hyper-V-Bak\' + $env:computername + '\'
-$DiffBackupFolder='G:\Hyper-V-Bak-Diff\' + $env:computername + '\'
+# ExportVMs.ps1 - Exports all virtual machines, deduplicates them and optionally copies the deduplicated ones to a different folder
+#
+# Par1 - New Backup Folder: "F:\Hyper-V-Bak\HYPERV3"
+# Par2 - (optional) Count of history backups to keep; older will be deleted: -1 (backup history not touched) or 5 (the newly created one and 5 older backups will be kept)
+# Par3 - (optional) Base Folder: if specified, "G:\Hyper-V-Bak\HYPERV3"
+# Par4 - (optional) Diff Backup Folder or "make_new_base": "make_new_base" or "G:\Hyper-V-Bak-Diff\HYPERV3"
+# Par5 - (optional) Count of history backup bases to keep; older will be deleted: -1 (base backup history not touched) or 1 (the newly created one and 1 older base backups will be kept)
 
-Write-Output "Starting Hyper-V backup job" | Out-File -Force "$LogFile"
-
-Write-Output "Output Folder: $ExportPath" | Out-File -Append "$LogFile"
-
-cd "$ExportPath"
+#[Parameter(Mandatory=$true)]
+Param (
+#   [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true)][String]$BackupFolder, # = "F:\Hyper-V-Bak\HYPERV3",
+    [Int]$HistoryKeep, # = -1,
+    [String]$DedupBase, # = "G:\Hyper-V-Bak\HYPERV3",
+    [String]$DiffBackupFolder, # = "make_new_base", #"G:\Hyper-V-Bak-Diff\HYPERV3",
+    [Int]$BaseHistoryKeep = 2
+)
 
 (get-process | ?{$_.ID -eq $pid}).priorityclass = "BelowNormal"
 
-$HistoryKeep=[int]5
 $Date=Get-Date -format "yyyyMMdd"
 $Time=Get-Date -format "HHmm"
 
-New-Item -Force "$ExportPath" -type directory | Out-Null
-$ExportPath="$ExportPath\$env:computername"
-New-Item -Force "$ExportPath" -Type directory | Out-Null
+# Create backup (sub)folders
+New-Item -Force "$BackupFolder" -Type directory | Out-Null
+$BackupFolder="$BackupFolder\$Date-$Time"
+New-Item -Force "$BackupFolder" -Type directory | Out-Null
 
-$HistoryFile="$ExportPath\history.txt"
+# Log Start
+$LogFile="$BackupFolder\log.txt"
+Write-Output "ExportVMs started at: $(Get-Date -format "HH:mm:ss")" | Tee-Object -FilePath "$LogFile"
 
-$ExportPath="$ExportPath\$Date-$Time"
-New-Item -Force "$ExportPath" -Type directory | Out-Null
-
-$LogFile="$ExportPath\log.txt"
-
-# Check backup count...
-$HistoryCnt = (Get-Content "$HistoryFile" | Measure-Object –Line).Lines
-if ($HistoryCnt -gt $HistoryKeep) {
-	# delete the oldest backup
-	$ToDelete=(Get-Content "$HistoryFile" -First 1)
-	Write-Output "Backup history: Deleting oldest backup: $ToDelete..." | Out-File -Append "$LogFile"
-	Remove-Item -Recurse -Force "$ToDelete" | Out-File -Append "$LogFile"
-	Write-Output " done" | Out-File -Append "$LogFile"
-
-	# Keep only $HistoryKeep backups
-	$ToKeep=(Get-Content "$HistoryFile" -Last $HistoryKeep)
-	Write-Output $ToKeep > $HistoryFile
+# Backup history
+if ($HistoryKeep -lt 0) {
+    Write-Output "Not touching previous backups..." | Tee-Object -Append -FilePath "$LogFile"
 } else {
-	Write-Output "Backup history: NOT enough backups in the history yet" | Out-File -Append "$LogFile"
+    # Check backup count
+    $HistoryFile="$BackupFolder\history.txt"
+    $HistoryCnt = (Get-Content "$HistoryFile" | Measure-Object –Line).Lines
+    if ($HistoryCnt -gt $HistoryKeep) {
+    	# delete the oldest backup
+    	$ToDelete=(Get-Content "$HistoryFile" -First 1)
+    	Write-Output "Backup history: Deleting oldest backup: $ToDelete..." | Tee-Object -Append -FilePath "$LogFile"
+    	Remove-Item -Recurse -Force "$ToDelete" | Out-File -Append "$LogFile"
+    	Write-Output " done" | Tee-Object -Append -FilePath "$LogFile"
+
+    	# Keep only $HistoryKeep backups
+    	$ToKeep=(Get-Content "$HistoryFile" -Last $HistoryKeep)
+    	Write-Output $ToKeep > $HistoryFile
+    } else {
+    	Write-Output "Backup history: NOT enough backups in the history yet" | Tee-Object -Append -FilePath "$LogFile"
+    }
 }
 
 Import-Module Hyper-V
-$VMs = Get-VM
+$VMs = Get-VM "User-NetBox"
 
-Write-Output "Starting at $(Get-Date -format "HH:mm:ss")"
-Write-Output "Starting at $(Get-Date -format "HH:mm:ss")" | Out-File -Append "$LogFile"
+Write-Output "Starting backup at $(Get-Date -format "HH:mm:ss")" | Tee-Object -Append -FilePath "$LogFile"
 foreach($VM in $VMs)
 {
-	#Exporting virtual machine to a local drive
-	$Begin=Get-Date
-	Write-Output ("VM: "+($VM.VMName)) | Out-File -Append "$LogFile"
-	Export-VM $VM -Path "$ExportPath"
-	Write-Output "   m $($(Get-Date) - $Begin)" | Out-File -Append "$LogFile"
+    #Exporting virtual machine to a local drive
+    $Begin=Get-Date
+    Write-Output ("VM: "+($VM.VMName)) | Tee-Object -Append -FilePath "$LogFile"
+    Export-VM $VM -Path "$BackupFolder"
+    Write-Output "   m $($(Get-Date) - $Begin)" | Tee-Object -Append -FilePath "$LogFile"
 }
 
-#Write-Output "Compressing at $(Get-Date -format "HH:mm:ss")"
-#Write-Output "Compressing at $(Get-Date -format "HH:mm:ss")" | Out-File -Append "$LogFile"
-#cd "$ExportPath"
+#Write-Output "Compressing at $(Get-Date -format "HH:mm:ss")" | Tee-Object -Append -FilePath "$LogFile"
+#cd "$BackupFolder"
 #foreach($VM in $VMs)
 #{
 #	# Compress a virtual machine
 #	$Begin=Get-Date
-#	Write-Output ("VM: "+($VM.VMName)) | Out-File -Append "$LogFile"
+#	Write-Output ("VM: "+($VM.VMName)) | Tee-Object -Append -FilePath "$LogFile"
 #	#&"c:\Program Files\WinRAR\WinRAR.exe" -m1 -mt8 m (($VM.VMName)+".rar") $VM.VMName
-#	&"c:\Program Files\WinRAR\RAR.exe" -m1 -mt8 m (($VM.VMName)+".rar") $VM.VMName | Out-File -Append "$LogFile"
-#	Write-Output "   c $($(Get-Date) - $Begin)" | Out-File -Append "$LogFile"
+#	&"c:\Program Files\WinRAR\RAR.exe" -m1 -mt8 m (($VM.VMName)+".rar") $VM.VMName | Tee-Object -Append -FilePath "$LogFile"
+#	Write-Output "   c $($(Get-Date) - $Begin)" | Tee-Object -Append -FilePath "$LogFile"
 #}
 
-Write-Output "Finished at $(Get-Date -format "HH:mm:ss")"
-Write-Output "Finished at $(Get-Date -format "HH:mm:ss")" | Out-File -Append "$LogFile"
-
 # When all successful => Add to history
-Write-Output "$ExportPath" >> $HistoryFile
+if ($HistoryKeep -lt 0) {
+    Write-Output "Not modifying backup history..." | Tee-Object -Append -FilePath "$LogFile"
+} else {
+    Write-Output "Adding `"$BackupFolder`" to backup history..." | Tee-Object -Append -FilePath "$LogFile"
+    Write-Output "$BackupFolder" >> $HistoryFile
+}
 
+# (optional) Deduplication postprocessing
+if (-not ([string]::IsNullOrEmpty($DiffBackupFolder))) {
+    $BaseHistoryFile="$DedupBase`\base_history.txt"
+    Write-Output "Deduplicating at $(Get-Date -format "HH:mm:ss")" | Tee-Object -Append -FilePath "$LogFile"
 
-# Deduplicate
-$BaseFolder=$DedupBase + (Get-Content ($BaseFolder + '\dedup_last_full.txt') -TotalCount 1)
-Write-Output "Dedup.ps1 $BaseFolder $ExportPath $DiffBackupFolder" | Out-File -Append "$LogFile"
-HyperVBackupDeduper.ps1 "$BaseFolder" "$ExportPath" "$DiffBackupFolder"
+    if ($DiffBackupFolder -ieq "make_new_base") {
+        # Make a new base folder
+        $BaseFolder="$DedupBase" + '\' + "$Date-$Time"
+        Write-Output "Hashing new base folder: $BaseFolder" | Tee-Object -Append -FilePath "$LogFile"
+        .\HyperVBackupDeduper.ps1 "$BackupFolder" | Tee-Object -Append -FilePath "$LogFile"
+        Write-Output "Creating new base folder: $BaseFolder" | Tee-Object -Append -FilePath "$LogFile"
+        Copy-Item -Recurse "$BackupFolder" -Destination "$BaseFolder"
 
-Write-Output "Deduplicated at $(Get-Date -format "HH:mm:ss")"
-Write-Output "Deduplicated at $(Get-Date -format "HH:mm:ss")" | Out-File -Append "$LogFile"
+        # Base backup history
+        if ($BaseHistoryKeep -lt 0) {
+            Write-Output "Not touching previous base backups..." | Tee-Object -Append -FilePath "$LogFile"
+        } else {
+            # Check base backup count
+            $HistoryCnt = (Get-Content "$BaseHistoryFile" | Measure-Object –Line).Lines
+            if ($HistoryCnt -gt $BaseHistoryKeep) {
+    	        # delete the oldest backup
+    	        $ToDelete=(Get-Content "$BaseHistoryFile" -First 1)
+    	        Write-Output "Base backup history: Deleting oldest backup: $ToDelete..." | Tee-Object -Append -FilePath "$LogFile"
+    	        Remove-Item -Recurse -Force "$DedupBase`\$ToDelete" | Out-File -Append "$LogFile"
+    	        Write-Output "Base backup history: done" | Tee-Object -Append -FilePath "$LogFile"
+
+    	        # Keep only $HistoryKeep backups
+    	        $ToKeep=(Get-Content "$BaseHistoryFile" -Last $BaseHistoryKeep)
+    	        Write-Output $ToKeep > $BaseHistoryFile
+            } else {
+    	        Write-Output "Base backup history: NOT enough backups in the history yet ($HistoryCnt <= $BaseHistoryKeep)" | Tee-Object -Append -FilePath "$LogFile"
+            }
+            # Write this new base to the base history file
+            Write-Output $Date-$Time >> $BaseHistoryFile
+        }
+
+    } else {
+        # Deduplicate
+        Write-Output "Deduplicating and copying deduplicated backups to folder: $DiffBackupFolder" | Tee-Object -Append -FilePath "$LogFile"
+        $BaseFolder=$DedupBase + '\' + (Get-Content "$BaseHistoryFile" -Last 1)
+        Write-Output ".\HyperVBackupDeduper.ps1 `"$BaseFolder`" `"$BackupFolder`" `"$DiffBackupFolder\$Date-$Time`"" | Tee-Object -Append -FilePath "$LogFile"
+        .\HyperVBackupDeduper.ps1 "$BackupFolder" "$BaseFolder" "$DiffBackupFolder\$Date-$Time" | Tee-Object -Append -FilePath "$LogFile"
+    }
+}
+
+Write-Output "Finished at $(Get-Date -format "HH:mm:ss")" | Tee-Object -Append -FilePath "$LogFile"
